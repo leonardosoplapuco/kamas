4dda24d0d5a747f93fd594212cb056bdfff22885
Leonardo Soplapuco <leonardosoplapuco@gmail.com>
2025-03-10 10:59:17 -0500

    0.0.6

--- src/index.js
+++ src/index.js
@@ -1,27 +1,3 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-
-// import { HelmetProvider } from "react-helmet-async";
-
-// import './App.css';
-
-// import App from './App';
-
-// ReactDOM.render(
-//     <HelmetProvider>
-//         <App/>
-//     </HelmetProvider>,
-
-//     document.getElementById("root")
-// );
-
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import App from "./App";
-
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(<App />);
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 
