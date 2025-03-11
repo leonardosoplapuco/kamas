4aacc01974de289a60c15796338cbe8171c0dc09
Leonardo Soplapuco <leonardosoplapuco@gmail.com>
2025-03-11 10:53:57 -0500

    0.1.6

--- src/Pages/Categories/Layout.js
+++ src/Pages/Categories/Layout.js
@@ -8,13 +8,13 @@ import Footer from '../../Componentes/Footer/Footer';
 import './Layout.css';
 
 function Layout() {
-    const { category } = useParams(); // Extrae el parámetro de la URL
+    const { category } = useParams();
     const capitalizedCategory = category ? category.charAt(0).toUpperCase() + category.slice(1) : '';
 
     const [meta, setMeta] = useState(null);
     const [filtros, setFiltros] = useState([]);
     const [productos, setProductos] = useState([]);
-    const [error, setError] = useState(null); // Nuevo estado para manejar errores
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         if (!category) {
@@ -22,9 +22,8 @@ function Layout() {
             return;
         }
 
-        setError(null); // Resetear error en caso de un nuevo intento
+        setError(null);
 
-        // Cargar metadatos
         fetch('/assets/JSON/Categories.json')
             .then((res) => res.json())
             .then((data) => {
@@ -38,7 +37,6 @@ function Layout() {
                 setError("No se pudo cargar la categoría.");
             });
 
-        // Cargar filtros
         fetch(`/assets/JSON/Productos/${capitalizedCategory}/Filtros.json`)
             .then((res) => res.json())
             .then((data) => setFiltros(data))
@@ -47,7 +45,6 @@ function Layout() {
                 setError("No se pudieron cargar los filtros.");
             });
 
-        // Cargar productos desde múltiples archivos
         Promise.all([
             fetch(`/assets/JSON/Productos/${capitalizedCategory}/Modelos/Adel.json`).then((res) => res.json()),
             fetch(`/assets/JSON/Productos/${capitalizedCategory}/Modelos/Sarki.json`).then((res) => res.json()),
@@ -82,6 +79,7 @@ function Layout() {
                     <div className="block-content">
                         <section className="category-container">
                             <div className="filters-container">
+                                <p>Filtrar por:</p>
                                 {filtros.map((filtro, index) => (
                                     <div key={`filtro-${index}`} className="filter">
                                         <h3>{filtro.name}</h3>
@@ -100,13 +98,31 @@ function Layout() {
                             <div className="products">
                                 {productos.map((producto, index) => {
                                     const uniqueKey = producto.id 
-                                        ? `producto-${producto.id}-${index}`
-                                        : `producto-${index}-${new Date().getTime()}`;
-                                    return (
+                                    ? `producto-${producto.id}-${index}`
+                                    : `producto-${index}-${new Date().getTime()}`;
+
+                                    const precioNormal = producto.precioNormal || 0;
+                                    const precioVenta = producto.precioVenta || 0;
+
+                                    const descuento = (precioNormal > 0 && precioVenta > 0)
+                                    ? Math.round(((precioNormal - precioVenta) * 100) / precioNormal)
+                                    : 0;
+
+                                    return(
                                         <a href={producto.ruta} key={uniqueKey} className="product">
-                                            <img src="" alt=""></img>
-                                            <h3>{producto.nombre}</h3>
-                                            <p>Precio: S/.{producto.precio}</p>
+                                            <div className='product-images'>
+                                                <img src={`${producto.fotos}1.jpg`} alt={producto.nombre} className='product-image product-image-1'/>
+                                                <img src={`${producto.fotos}2.jpg`} alt={producto.nombre} className='product-image product-image-2'/>
+                                                <span className='product-label-discount'>-{descuento}%</span>
+                                            </div>
+                                            <div className='product-description'>
+                                                <p className='product-brand'>KAMAS</p>
+                                                <p className='product-name'>{producto.nombre}</p>
+                                                <div className='product-pryces'>
+                                                    <p className='product-pryce-1'>S/.{precioNormal}</p>
+                                                    <p className='product-pryce-2'>S/.{precioVenta}</p>
+                                                </div>
+                                            </div>
                                         </a>
                                     );
                                 })}
